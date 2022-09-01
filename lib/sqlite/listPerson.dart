7baf848ca2c36a8f404ee16f033733aa6c9bab64
GuilhermeSantosFerrace <guilherme-ferrace@hotmail.com>
2022-09-01 20:02:08 -0300

    Concluindo SQLite

--- lib/sqlite/listPerson.dart
+++ lib/sqlite/listPerson.dart
@@ -29,6 +29,26 @@ class _ListPersonState extends State<ListPerson>{
     });
   }
 
+  insertPerson(Person person) async {
+    int id = await PersonDAO().insertPerson(person);
+    person.id = id;
+    if(id > 0) {
+      setState((){
+        persons.add(person);
+      });
+    }
+  }
+
+  deletePerson(int index) async {
+    Person person = persons[index];
+    if(person.id != null) {
+      await PersonDAO().deletePersonById(person.id!);
+      setState(() {
+        persons.add(person);
+      });
+    }
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -39,21 +59,24 @@ class _ListPersonState extends State<ListPerson>{
             Navigator.push(context,
               MaterialPageRoute(builder:(context) => AddPerson()))
               .then((person) => {
-                setState((){
-                  persons.add(person);
-                })
+                insertPerson(person)
               });
           },
           icon: const Icon(Icons.add),
           )
         ],
         ),
-      body: ListView(children: buildListItens()),
+      body: ListView.separated(
+          itemCount: persons.length,
+          itemBuilder: (context, index) => buildListItens(index),
+          separatorBuilder: (context, index) => const Divider( height: 1),
+      ),
     );
   }
 
-  List<Widget> buildListItens(){
-    return persons.map((p) => Padding(
+  Widget buildListItens(int index){
+    Person p = persons[index];
+    return Padding(
       padding: const EdgeInsets.symmetric(horizontal: 16.0, vertical: 8.0),
       child: Container(
         decoration: BoxDecoration(
@@ -64,8 +87,11 @@ class _ListPersonState extends State<ListPerson>{
           leading: Text(p.id != null ? p.id.toString() : "-1"),
           title: Text(p.firstName),
           subtitle: Text(p.lastName),
+          onLongPress: (){
+            deletePerson(index);
+          },
         ),
       ),
-      )).toList();
+      );
   }
 }
